5d46111b3890aa986e314de5015b885a9bc37c72
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-13 15:31:57 +0200

    test

--- js/main.js
+++ js/main.js
@@ -2,7 +2,23 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            
-        }
+            mailList: '',
+        },
+        methods: {
+            generateTenMail: function () {
+
+                for (let index = 0; index < 10; index++) {
+                    
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    
+                        .then( (response) => {
+    
+                            this.mail = response.data.response;
+                            
+                            console.log(this.mail);
+                        });
+                };
+            }
+        },
     }
 );
